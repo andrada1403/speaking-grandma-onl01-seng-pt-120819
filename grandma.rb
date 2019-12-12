c58e425ef0ac5c3ff4641ae202282f2aad22f0a1
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-12-12 19:26:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(statement)
-  if ""
+  if "I LOVE YOU GRANDMA!"
+    puts "" 
+  end
 end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
